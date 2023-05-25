08dbe6d017bbae403a080ed2c8271882331bc2bd
MetyEstalu <metyswety@yahoo.com>
2023-05-25 02:29:58 -0700

    task0-3

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,27 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
-/**
-* bubble_sort - a function that sorts an array of integers in
-* ascending order using the Bubble sort algorithm
-* @array: array of integers
-* @size: size of array
-**/
 
+/**
+ *bubble_sort - sorts an array of integers in ascending orde
+ *@array: the array to be sort
+ *@size: the size of the array
+ */
 void bubble_sort(int *array, size_t size)
 {
-size_t c, j;
-int temp;
-for (c = 0; c < size; c++)
-{
-for (j = 0; j < size - 1; j++)
-{
-if (array[j] > array[j + 1])
-{
-temp = array[j];
-array[j] = array[j + 1];
-array[j + 1] = temp;
-print_array(array, size);
-}
-}
+	size_t j, i;
+	int n;
+
+	if (size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				n = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = n;
+				print_array(array, size);
+			}
+		}
+	}
 }
--- sort.h
+++ sort.h
@@ -1,13 +1,16 @@
-#ifndef SORT_HEADER
-#define SORT_HEADER
+#ifndef _SORTING_H_
+#define _SORTING_H_
 #include <stdio.h>
-#include <stdlib.h>
+
+
+/** Struct Double Linked List */
 
 /**
  * struct listint_s - Doubly linked list node
+ *
  * @n: Integer stored in the node
- * @next: Pointer to the next node of the list
- * @prev: Pointer to the previous node of the list
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
@@ -16,13 +19,17 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void swap(int *arr, int idx1, int idx2);
+/** Given FUN prototype */
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+/** TASK Prototype */
 void bubble_sort(int *array, size_t size);
-listint_t *create_listint(const int *array, size_t size);
-void swap_list(listint_t *pn, listint_t *temp, listint_t *nxt, listint_t **h);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
 #endif
